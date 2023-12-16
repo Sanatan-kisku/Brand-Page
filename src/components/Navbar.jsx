5c8415d88ad7295c5408bcf4313147e61b3ddf58
Sanatan Kisku <sanatankisku@gmail.com>
2023-12-16 19:16:04 +0530

    Added Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+      <div className='logo'>
+        <img src="/images/brand_logo.png" alt="brand-logo" />
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+      </ul>
+      <button className='btn'>Login</button>
+      </nav>
+    </div>
+  )
+}
