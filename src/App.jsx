0ec0fe03de3e9687bf7d83d6b45f28f0332daf60
Sanatan Kisku <sanatankisku@gmail.com>
2023-12-16 21:59:44 +0530

    Added and imported hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Navbar from './components/Navbar'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
+import Hero from './components/Hero'
 
 function App() {
   return (
-    <>
-      <Navbar/>
-    </>
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
   )
 }
 
